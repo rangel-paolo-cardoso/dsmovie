bc155da9430c3a58d02ce76f779c4802b83e9be6
Rangel Paolo <paolorangel1996@outlook.com>
2022-05-15 14:41:39 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,13 +1,17 @@
 import MovieStars from '../MovieStars';
 import './styles.css';
 
-export default function index() {
-  const score = 3.5;
-  const count = 15;
+type Props = {
+  score: number;
+  count: number;
+}
+
+export default function index({ count, score }: Props) {
+  
   return (
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
